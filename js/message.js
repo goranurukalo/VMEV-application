ba8e265e9c3137d320c664dbeda10e2f456b3ff2
goranurukalo <dev.goranurukalo@gmail.com>
2017-04-14 18:50:57 +0200

    This is big part of project , its almost done :D

--- js/message.js
+++ js/message.js
@@ -1,7 +1,13 @@
-$('.chat[data-chat=person2]').addClass('active-chat');
-$('.person[data-chat=person2]').addClass('active');
+$('.chat[data-chat=person3]').addClass('active-chat');
+$('.person[data-chat=person3]').addClass('active');
 
 $('.left .person').mousedown(function(){
+	//
+	// If tool == message go on , else fail
+	//
+	if(!$(".features .tools li[data-tool=message]").hasClass('tool-active')){
+		return false;
+	}
     if ($(this).hasClass('.active')) {
         return false;
     } else {
@@ -12,5 +18,27 @@ $('.left .person').mousedown(function(){
         $('.left .person').removeClass('active');
         $(this).addClass('active');
         $('.chat[data-chat = '+findChat+']').addClass('active-chat');
+		
+		$(".active-chat").scrollTop($(".active-chat")[0].scrollHeight);
+		
+		animation();
     }
 });
+
+function animation(){
+	if($(".right .active-chat .bubble:nth-child(2)").attr("style") != undefined){
+		//
+		//	this is for performanse
+		//	if he got duration dont calc it again
+		//
+		return false;
+	}
+	
+	var number = $('.right .active-chat').children().length - 1;
+	$(".right .active-chat .bubble").each(function(index){
+		$(this).css({
+			'animation-duration' : 0.1*(1+ number - index) + 's'
+		});
+	});
+}
+animation();
